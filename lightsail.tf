5f58113bb894f674d9664b5bea9e66877479416f
Patrick <patricktshibangu@gmail.com>
2023-09-17 15:53:38 -0400

    ticket number

---
+++ lightsail.tf
@@ -0,0 +1,17 @@
+resource "aws_lightsail_instance" "gitlab_test" {
+  name              = "custom_gitlab"
+  availability_zone = "us-east-1a"
+  blueprint_id      = "amazon_linux_2"
+  bundle_id         = "nano_1_0"
+  key_pair_name     = "some_key_name"
+  tags = {
+    foo = "bar"
+  }
+}
+user_data = <<-EOF
+              #!/bin/bash
+              sudo yum install httpd -y
+              sudo systemctl start httpd
+              sudo systemctl enable httpd
+              echo "<h1>This Server is created using Terraform </h1>" | sudo tee /var/www/html/index.html
+              EOF
