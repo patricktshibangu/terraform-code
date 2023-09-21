2558811f36a1fb37254f9a113f8fd37348d3d036
Patrick <patricktshibangu@gmail.com>
2023-09-21 14:34:44 -0400

    little change on region

--- lightsail.tf
+++ lightsail.tf
@@ -1,6 +1,6 @@
 resource "aws_lightsail_instance" "gitlab_test" {
   name              = "custom_gitlab"
-  availability_zone = "us-east-1a"
+  availability_zone = "us-east-1"
   blueprint_id      = "amazon_linux_2"
   bundle_id         = "nano_1_0"
   key_pair_name     = "some_key_name"
